daf9d47bb93a1092635da3821eca77ac4a6b6183
Fernando O. Gallego <fogallego@us.es>
2017-12-20 14:07:17 +0100

    Added new models.

--- model_factory.py
+++ model_factory.py
@@ -4,7 +4,7 @@ import numpy as np
 from keras import Input
 from keras.callbacks import EarlyStopping
 from keras.engine import Model
-from keras.layers import Dense, Dropout, Masking, Flatten
+from keras.layers import Dense, Dropout, Masking, Flatten, Conv1D, GlobalMaxPooling1D, GRU, Bidirectional, MaxPooling1D
 from keras.preprocessing import sequence
 
 
@@ -187,6 +187,362 @@ class MLPSoftmax(ModelBase):
         x = Flatten()(x)
         preds = Dense(1, activation='softmax')(x)
         self.model = Model(sequence_input, preds, name='MLPSoftmax')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class MLPSigmoid2(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = Dense(50, activation='tanh')(embedded_sequences)
+        x = (Dropout(0.2))(x)
+        x = Flatten()(x)
+        x = Dense(50, activation='tanh')(x)
+        preds = Dense(1, activation='sigmoid')(x)
+        self.model = Model(sequence_input, preds, name='MLPSigmoid2')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class MLPRelu2(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = Dense(50, activation='tanh')(embedded_sequences)
+        x = (Dropout(0.2))(x)
+        x = Flatten()(x)
+        x = Dense(50, activation='tanh')(x)
+        preds = Dense(1, activation='relu')(x)
+        self.model = Model(sequence_input, preds, name='MLPRelu2')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class CNNRelu(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (Conv1D(filters=128, kernel_size=3, activation='relu'))(embedded_sequences)
+        # x = (AveragePooling1D(pool_size=3))(x)
+        x = (Dropout(0.2))(x)
+        x = (Conv1D(filters=32, kernel_size=17, activation='relu'))(x)
+        x = (Dropout(0.2))(x)
+        x = (GlobalMaxPooling1D())(x)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='relu')(x)
+        self.model = Model(sequence_input, preds, name='CNNRelu')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class CNNSigmoid(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (Conv1D(filters=128, kernel_size=3, activation='relu'))(embedded_sequences)
+        # x = (AveragePooling1D(pool_size=3))(x)
+        x = (Dropout(0.2))(x)
+        x = (Conv1D(filters=32, kernel_size=17, activation='relu'))(x)
+        x = (Dropout(0.2))(x)
+        x = (GlobalMaxPooling1D())(x)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='sigmoid')(x)
+        self.model = Model(sequence_input, preds, name='CNNSigmoid')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class GRURelu(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (GRU(50, dropout=0.15))(embedded_sequences)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='relu')(x)
+        self.model = Model(sequence_input, preds, name='GRURelu')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class GRUSigmoid(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (GRU(50, dropout=0.15))(embedded_sequences)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='sigmoid')(x)
+        self.model = Model(sequence_input, preds, name='GRUSigmoid')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class BiGRURelu(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (Bidirectional(GRU(50, dropout=0.15)))(embedded_sequences)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='relu')(x)
+        self.model = Model(sequence_input, preds, name='BiGRURelu')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class BiGRUSigmoid(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (Bidirectional(GRU(50, dropout=0.15)))(embedded_sequences)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='sigmoid')(x)
+        self.model = Model(sequence_input, preds, name='GRUSigmoid')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class CNNBiGRURelu(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (Conv1D(filters=32, kernel_size=3, activation='relu'))(embedded_sequences)
+        x = (MaxPooling1D(pool_size=2))(x)
+        x = (Bidirectional(GRU(100, dropout=0.15)))(x)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='relu')(x)
+        self.model = Model(sequence_input, preds, name='CNNBiGRURelu')
+        self.compile_model()
+
+    def compile_model(self):
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
+
+    def fit(self, x, y, batch_size, epochs, verbose, callbacks):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        y_preprocessed = np.array(y)
+
+        self.model.fit(x_preprocessed, y_preprocessed, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                       callbacks=callbacks)
+
+    def predict(self, x):
+        x_preprocessed = np.array(x)
+        x_preprocessed = sequence.pad_sequences(x_preprocessed, maxlen=self.maxlen, value=-1)
+
+        return self.model.predict(x_preprocessed)
+
+
+class CNNBiGRUSigmoid(ModelBase):
+    def __init__(self, wv, maxlen=50, max_num_deptag=50):
+        super().__init__(wv, maxlen, max_num_deptag)
+        embedding_layer = self.wv.model.wv.get_embedding_layer()
+
+        sequence_input = Input(shape=(self.maxlen,), dtype='int32')
+        mask = Masking(mask_value=-1)(sequence_input)
+        embedded_sequences = embedding_layer(mask)
+        x = (Conv1D(filters=32, kernel_size=3, activation='relu'))(embedded_sequences)
+        x = (MaxPooling1D(pool_size=2))(x)
+        x = (Bidirectional(GRU(100, dropout=0.15)))(x)
+        x = (Dense(16))(x)
+        x = (Dropout(0.2))(x)
+        preds = Dense(1, activation='sigmoid')(x)
+        self.model = Model(sequence_input, preds, name='CNNGRUSigmoid')
         self.compile_model()
 
     def compile_model(self):
